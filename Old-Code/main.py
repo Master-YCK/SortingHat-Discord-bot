522d56afa71d20b8a38beab1bb9575b685dff3a7
JohnYu <23225157@life.hkbu.edu.hk>
2024-01-23 17:46:49 +0800

    re-dev setup

--- Old-Code/main.py
+++ Old-Code/main.py
@@ -1,114 +1,114 @@
-import discord
-import response
-import chatgpt_response
-import datetime
-import os
-from discord.ext import commands
+# import discord
+# import response
+# import chatgpt_response
+# import datetime
+# import os
+# from discord.ext import commands
 
-# Discord bot token
-TOKEN = "MTE4NTg1MTUwOTEyNTE2OTIxMg.GVV6un.aaWm2D92FXaEyc1SkY9CATdlUcZhFV62a0_MEE"
-# Setting the bot permession
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
+# # Discord bot token
+# TOKEN = "MTE4NTg1MTUwOTEyNTE2OTIxMg.GVV6un.aaWm2D92FXaEyc1SkY9CATdlUcZhFV62a0_MEE"
+# # Setting the bot permession
+# intents = discord.Intents.default()
+# intents.message_content = True
+# intents.members = True
 
-#Set you welcome channel
-welcome_channel = 1071065748631466024
+# #Set you welcome channel
+# welcome_channel = 1071065748631466024
 
-client = commands.Bot(command_prefix="./", intents=intents)
+# client = commands.Bot(command_prefix="./", intents=intents)
 
-# Bot start message
-def run():
-    @client.event
-    async def on_ready():
-        print(f"{client.user} is now running!")
+# # Bot start message
+# def run():
+#     @client.event
+#     async def on_ready():
+#         print(f"{client.user} is now running!")
 
-    #Welcome message and auto assign a user role
-    @client.event
-    async def on_member_join(member):
-        channel = client.get_channel(welcome_channel)
-        role = discord.utils.get(member.guild.roles, name = "麻瓜")
-        await member.add_roles(role) 
+#     #Welcome message and auto assign a user role
+#     @client.event
+#     async def on_member_join(member):
+#         channel = client.get_channel(welcome_channel)
+#         role = discord.utils.get(member.guild.roles, name = "麻瓜")
+#         await member.add_roles(role) 
 
-        embed = discord.Embed(
-            title = f'**Welcome to Hogwarts !**',
-            description = f'**{member.mention}** come to the Hogwarts, but he/she is a **{role.name}** !!!',
-            color = 0xff55ff,
-            timestamp = datetime.datetime.now()
-        )
+#         embed = discord.Embed(
+#             title = f'**Welcome to Hogwarts !**',
+#             description = f'**{member.mention}** come to the Hogwarts, but he/she is a **{role.name}** !!!',
+#             color = 0xff55ff,
+#             timestamp = datetime.datetime.now()
+#         )
 
-        await channel.send(embed = embed)
+#         await channel.send(embed = embed)
 
-    @client.command()
-    async def HELP(ctx):
-        help_embed = discord.Embed(
-            title = f'**THIS IS HELP GUIDE TO QUICK START ON BOT**',
-            description = "**Command List**\n ./HELP **:** Help Message\n ./myrole **:** Check you own user role\n ./talk XXXX **:** Talk with the bot"
-        )
-        await ctx.send(embed = help_embed)
+#     @client.command()
+#     async def HELP(ctx):
+#         help_embed = discord.Embed(
+#             title = f'**THIS IS HELP GUIDE TO QUICK START ON BOT**',
+#             description = "**Command List**\n ./HELP **:** Help Message\n ./myrole **:** Check you own user role\n ./talk XXXX **:** Talk with the bot"
+#         )
+#         await ctx.send(embed = help_embed)
 
-    @client.command()  # 使用 commands.command() 装饰器定义命令
-    async def chat(ctx, *, user_input):
-        response = chatgpt_response.get_chat_response(user_input)
-        await ctx.send(response)
+#     @client.command()  # 使用 commands.command() 装饰器定义命令
+#     async def chat(ctx, *, user_input):
+#         response = chatgpt_response.get_chat_response(user_input)
+#         await ctx.send(response)
 
-    # async def on_member_join(member):
-    #     # 发送欢迎消息
-    #     await member.send("欢迎加入！请回答以下五个问题，以帮助我们了解你。")
+#     # async def on_member_join(member):
+#     #     # 发送欢迎消息
+#     #     await member.send("欢迎加入！请回答以下五个问题，以帮助我们了解你。")
 
-    #     # 提问五个问题
-    #     answers = []
-    #     for i in range(5):
-    #         question = f"问题 {i+1}: "
-    #         await member.send(question)
+#     #     # 提问五个问题
+#     #     answers = []
+#     #     for i in range(5):
+#     #         question = f"问题 {i+1}: "
+#     #         await member.send(question)
             
-    #         def check(msg):
-    #             return msg.author == member and msg.channel.type == discord.ChannelType.private
+#     #         def check(msg):
+#     #             return msg.author == member and msg.channel.type == discord.ChannelType.private
 
-    #         response = await client.wait_for('message', check=check)
-    #         answers.append(response.content)
+#     #         response = await client.wait_for('message', check=check)
+#     #         answers.append(response.content)
 
-    #     # 根据回答进行身份分组
-    #     role_name = get_group_based_on_answers(answers)  # 请自行实现这个函数
-    #     role = discord.utils.get(member.guild.roles, name=role_name)
+#     #     # 根据回答进行身份分组
+#     #     role_name = get_group_based_on_answers(answers)  # 请自行实现这个函数
+#     #     role = discord.utils.get(member.guild.roles, name=role_name)
 
-    #     if role:
-    #         await member.add_roles(role)
-    #         await member.send(f"你已被分配到 {role_name} 组！")
+#     #     if role:
+#     #         await member.add_roles(role)
+#     #         await member.send(f"你已被分配到 {role_name} 组！")
 
-    # def get_group_based_on_answers(answers):
-    #     # 这是一个示例函数，根据回答返回身份组的名称
-    #     # 请根据你的需求实现具体的逻辑
-    #     if answers.count("Yes") >= 3:
-    #         return "GroupA"
-    #     else:
-    #         return "GroupB"
+#     # def get_group_based_on_answers(answers):
+#     #     # 这是一个示例函数，根据回答返回身份组的名称
+#     #     # 请根据你的需求实现具体的逻辑
+#     #     if answers.count("Yes") >= 3:
+#     #         return "GroupA"
+#     #     else:
+#     #         return "GroupB"
 
-    # Server user rock check (Self check)
-    @client.command()
-    async def myrole(ctx):
-        user = ctx.author
-        roles = [role.name for role in user.roles if not role.is_default()]
-        if not roles:
-            await ctx.send(f"{ctx.author.mention} doesn't have any roles.")
-        else:
-            await ctx.send(f"{ctx.author.mention} has the following roles: {', '.join(roles)}")
+#     # Server user rock check (Self check)
+#     @client.command()
+#     async def myrole(ctx):
+#         user = ctx.author
+#         roles = [role.name for role in user.roles if not role.is_default()]
+#         if not roles:
+#             await ctx.send(f"{ctx.author.mention} doesn't have any roles.")
+#         else:
+#             await ctx.send(f"{ctx.author.mention} has the following roles: {', '.join(roles)}")
 
-    # Message response
-    @client.command()
-    async def talk(ctx, *, message):
-        response_text = response.handle_response(message)
+#     # Message response
+#     @client.command()
+#     async def talk(ctx, *, message):
+#         response_text = response.handle_response(message)
 
-        if response_text == "Sor, what 7 you said ar ???":
-            image_url = "images\what 7 you said.jpg"
-            image_file = discord.File(image_url)
-            await ctx.send(f"{ctx.author.mention}, {response_text}", file = image_file)
-        else:
-            await ctx.send(f"{ctx.author.mention}, {response_text}")
+#         if response_text == "Sor, what 7 you said ar ???":
+#             image_url = "images\what 7 you said.jpg"
+#             image_file = discord.File(image_url)
+#             await ctx.send(f"{ctx.author.mention}, {response_text}", file = image_file)
+#         else:
+#             await ctx.send(f"{ctx.author.mention}, {response_text}")
 
-    # Run the bot with you own token
-    client.run(TOKEN)
+#     # Run the bot with you own token
+#     client.run(TOKEN)
 
- # run the bot
-if __name__ == "__main__":
-    run()
+#  # run the bot
+# if __name__ == "__main__":
+#     run()
---
+++ setting.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
