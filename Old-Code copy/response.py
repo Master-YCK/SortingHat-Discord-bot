7af15194b2d4d25a613b7870748e33695b79065b
JohnYu <23225157@life.hkbu.edu.hk>
2024-01-25 13:11:29 +0800

    file change

---
+++ Old-Code copy/response.py
@@ -0,0 +1,15 @@
+# # Bot message response 
+# def handle_response(message) -> str:
+#     # change any input into lower char
+#     p_message = message.lower()
+
+#     if p_message == "hello":
+#         return "Hello !"
+    
+#     if p_message == "hi":
+#         return "Hi !"
+    
+#     if p_message == "how are you":
+#         return "I'm great! How about you?"
+    
+#     return "Sor, what 7 you said ar ???"
