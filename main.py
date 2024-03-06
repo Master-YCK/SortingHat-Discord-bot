a251154a68297047b5a7691cd0c649513715586a
John Yu <23225157@life.hkbu.edu.hk>
2024-03-06 20:22:45 +0800

    Bug Fix and Function Update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import datetime
 import random
 import os
 
-
+# The button view class
 class SimpleView(discord.ui.View):
 
     foo: bool = None
@@ -26,18 +26,11 @@ class SimpleView(discord.ui.View):
     async def on_timeout(self) -> None:
         return
 
-    # @discord.ui.button(label="Hello", style=discord.ButtonStyle.success)
-    # async def hello(self, interaction: discord.Interaction, button: discord.ui.Button):
-    #     await interaction.response.send_message("World!")
-    #     self.foo = True
-    #     self.stop()
-
     @discord.ui.button(label="Retry", style=discord.ButtonStyle.red)
     async def retry(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message("The Retry Generate: ")
         self.foo = True
         self.stop()
-
 
 # Main function
 def run():
@@ -78,20 +71,22 @@ def run():
 
     # Server user rock check (Self check)
     @hat.command()
-    async def myrole(ctx):
+    async def myrole(ctx: commands.Context):
         user = ctx.author
         roles = [role.name for role in user.roles if not role.is_default()]
         if not roles:
-            await ctx.send(f"{ctx.author.mention} doesn't have any roles.")
+            async with ctx.typing():
+                await ctx.reply(f"{ctx.author.mention} doesn't have any roles.")
         else:
-            await ctx.send(
-                f"{ctx.author.mention} has the following roles: {', '.join(roles)}"
-            )
+            async with ctx.typing():
+                await ctx.reply(
+                    f"{ctx.author.mention} has the following roles: {', '.join(roles)}"
+                )
         print(f"({ctx.author}) check the role")
 
     # User message response
     @hat.command()
-    async def talk(ctx, *, user_input):
+    async def talk(ctx: commands.Context, *, user_input):
         bot_response = response.handle_response(user_input)
         # Console log to check user input
         print(f"({ctx.author}) said: [{user_input}] to the bot")
@@ -99,40 +94,35 @@ def run():
         if bot_response == None:
             image_path = "images/what 7 you said.jpg"
             image_file = discord.File(image_path)
-            await ctx.send(
-                f"{ctx.author.mention}, Sor, What 7 You Said Ar???", file=image_file
-            )
+            async with ctx.typing():
+                await ctx.reply(
+                    f"{ctx.author.mention}, Sor, What 7 You Said Ar???", file=image_file
+                )
             print(f"Bot response to ({ctx.author}) with no understand input text")
         else:
-            await ctx.send(f"{ctx.author.mention}, {bot_response}")
+            async with ctx.typing():
+                await ctx.reply(f"{ctx.author.mention}, {bot_response}")
             print(f"Bot response to ({ctx.author}) with [{bot_response}]")
-
-    @hat.command()
-    async def response(ctx, input):
-        async with ctx.typing():
-            await asyncio.sleep(3)
-        await ctx.send(f"Hello, {ctx.author.mention}! You said: {input}")
         
     # Roll the dice game
     @hat.command()
-    async def roll(ctx):
+    async def roll(ctx: commands.Context):
         image_dir = "images/dice/"
         dice_image = os.listdir(image_dir)
         image_path = image_dir + random.choice(dice_image)
         image_file = discord.File(image_path)
-        await ctx.send(
-            f"Congratulation !!! {ctx.author.mention} you roll the number:",
-            file=image_file,
-        )
+        async with ctx.typing():
+            await ctx.reply(
+                f"Congratulation !!! {ctx.author.mention} you roll the number:",
+                file=image_file,
+            )
         print(f"({ctx.author}) roll the dice with [{image_path}]")
 
     # Chat with the bot using the Google Gamme 2b Model
     @hat.command()
     async def chat(ctx: commands.Context, *, user_input):
         async with ctx.typing():
-            await asyncio.sleep(6)
-
-        bot_response = gamme.genText(user_input)
+            bot_response = gamme.genText(user_input)
 
         view = SimpleView(timeout=5)
 
@@ -147,14 +137,12 @@ def run():
             print("No button was clicked")
         elif view.foo is True:
             async with ctx.typing():
-                await asyncio.sleep(6)
-            bot_response = gamme.genText(user_input)
+                bot_response = gamme.genText(user_input)
             await ctx.reply(f"{ctx.author.mention}, {bot_response}", ephemeral=True)
             print(f"({ctx.author}) retry to generate the text context: [{bot_response}]")
             
     # Run the bot with the your discord API
     hat.run(setting.DISCORD_API_SECRET)
-
 
 if __name__ == "__main__":
     run()
